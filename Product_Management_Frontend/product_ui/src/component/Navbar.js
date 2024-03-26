a1357a559decbf69b89955ca5b241e2ee1b4dd9b
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-26 20:39:46 +0100

    Changed "class" to "className" in Navbar.js

--- Product_Management_Frontend/product_ui/src/component/Navbar.js
+++ Product_Management_Frontend/product_ui/src/component/Navbar.js
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Product Management System</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Product Management System</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-        <li class="nav-item">
-          <Link to="/" class="nav-link active" aria-current="page" href="#">Home</Link>
+        <li className="nav-item">
+          <Link to="/" className="nav-link active" aria-current="page" href="#">Home</Link>
         </li>
-        <li class="nav-item">
-            <Link to="/addProduct" class="nav-link active" aria-current="page" href="#">Add Product</Link>
+        <li className="nav-item">
+            <Link to="/addProduct" className="nav-link active" aria-current="page" href="#">Add Product</Link>
         </li>
-        <li class="nav-item">
-            <Link to="/editProduct" class="nav-link active" aria-current="page" href="#">Edit Product</Link>
+        <li className="nav-item">
+            <Link to="/editProduct" className="nav-link active" aria-current="page" href="#">Edit Product</Link>
         </li>
 
         
